d4349990530430174b29c384afa3eee4fa965f2c
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-03-17 03:39:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# Example Code
+
+number1 = int(input("Please enter a number"))
+number2 = int(input("Please enter another number"))
+
+if number1 > number2:
+  print("Number 1 is bigger than number 2")
+elif number2 > number1: 
+  print("Number 2 is bigger than number 1")
+else:
+  print("Both numbers are the same")
+
+
+# What line does selection start on?
+  # Answer: Line 6
+
+# How many selection statements are there in the code?
+  # Answer: 3 selection statements 
+
+# How many conditions are there in the code?
+  # Answer: 2 conditions
+
+# What does the > operator mean?
+  # Answer: ">" means greater than
+
+# Why are lines7, 9 and 11 indented?
+  # Answer: Those are the code that will run if the selection statements above them are true
+
+# What is the purpose of this program?
+  # Answer: To compare the 2 whole number inputs and print which one is bigger or if they are the same
+
+
+#Jaunel Deans
+#October 24, 2023
+#We used comments to answer the investigate questions on the example code and explored what the code does. 
